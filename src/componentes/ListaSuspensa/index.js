b3feaee1a90db730a8365bb9d65bc6d8fe44a3a0
Emanuel Marcos dos Santos Barboza <emanuel@tigrara.local>
2023-05-04 17:54:24 -0300

    2/3 formulário

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,19 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    return(
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select>
+                {props.itens.map(item => {
+                // Esse bloco de código para cada item ele vai colocar uma option
+                    // <option>{item}</option>
+                    // Ou
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    );
+}
+
+export default ListaSuspensa;
